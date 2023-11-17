d6fc7d8763d98761ecda08815c8e306b64867ce1
Pedro Pastor <5apeterpastor@gmail.com>
2023-11-16 23:46:21 -0600

    JSON Menu

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,35 @@
 const baseURL = "https://peter0226.github.io/wdd230/";
 const linksURL = "https://peter0226.github.io/wdd230/data/links.json";
+const linksContent = document.querySelector('#links');
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    displayLinks(data.lessons);
   }
   
   getLinks();
+
+
+  const displayLinks = (lessons) => {
+    lessons.forEach((lesson) => {
+        let week = document.createElement('li');
+        week.textContent = `Week ${lesson.lesson}: `;
+        lesson.links.forEach((link, index) => {
+        
+            let newLink = document.createElement('a'); 
+
+            newLink.href = link.url;
+            newLink.textContent = link.title;
+            if (link.type === "external") {
+                newLink.target = "_blank";
+            }
+        
+            week.appendChild(newLink); 
+            linksContent.appendChild(week);
+            if (index < lesson.links.length - 1) {
+                week.appendChild(document.createTextNode(" | "));
+            }
+        });
+    });
+  }
