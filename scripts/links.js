895502666093c5a7d53e9984e709925d1e908f28
Pedro Pastor <5apeterpastor@gmail.com>
2023-11-16 23:07:15 -0600

    JSON data

---
+++ scripts/links.js
@@ -0,0 +1,10 @@
+const baseURL = "https://peter0226.github.io/wdd230/";
+const linksURL = "https://peter0226.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    console.log(data);
+  }
+  
+  getLinks();
